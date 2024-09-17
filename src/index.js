907d7a85b03fff534525fdeb7d480dc50c3d5580
Allizaveta <allizaveta@gmail.com>
2024-09-17 13:49:19 +0300

    1

--- src/index.js
+++ src/index.js
@@ -1,12 +1,53 @@
 const http = require('http');
+const { getUsers } = require('./modules/users');
+
+const hostname = '127.0.0.1';
+const port = 3003;
 
 const server = http.createServer((request, response) => {
+    const url = new URL(request.url, `http://${hostname}:${port}`);
+    const name = url.searchParams.get('hello');
+    const usersParam = url.searchParams.get('users');
 
-    // Написать обработчик запроса:
-    // - Ответом на запрос `?hello=<name>` должна быть **строка** "Hello, <name>.", код ответа 200
-    // - Если параметр `hello` указан, но не передано `<name>`, то ответ **строка** "Enter a name", код ответа 400
-    // - Ответом на запрос `?users` должен быть **JSON** с содержимым файла `data/users.json`, код ответа 200
-    // - Если никакие параметры не переданы, то ответ **строка** "Hello, World!", код ответа 200
-    // - Если переданы какие-либо другие параметры, то пустой ответ, код ответа 500
+    response.setHeader('Content-Type', 'text/plain');
 
+    switch (true) {
+        // Если передан параметр hello с именем
+        case name !== null && name !== '':
+            response.statusCode = 200;
+            response.end(`Hello, ${name}`);
+            break;
+
+        // Если передан параметр hello, но имя пустое
+        case name !== null && name === '':
+            response.statusCode = 400;
+            response.end('Enter a name');
+            break;
+
+        // Если передан параметр users
+        case usersParam !== null:
+            getUsers((data) => {
+
+                response.statusCode = 200;
+                response.setHeader('Content-Type', 'application/json');
+                response.end(data);
+            })
+            break;
+
+        // Если параметры не переданы
+        case url.searchParams.toString() === '':
+            response.statusCode = 200;
+            response.end('Hello, World!');
+            break;
+
+        // Если переданы другие параметры
+        default:
+            response.statusCode = 500;
+            response.end();
+            break;
+    }
+});
+
+server.listen(port, hostname, () => {
+    console.log(`Сервер запущен по адресу http://${hostname}:${port}/`);
 });
